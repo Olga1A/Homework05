c5678cd5f073ef5a5ca7e29a0bb38ca9c368a2db
Olga <aleksandrova1olga@gmail.com>
2024-05-12 22:30:37 +0300

    Homework05

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Book {
-}
+public class Book {
+
+    //declaring fields
+    String title;
+    String author;
+    long ISBN;
+
+    //constractor with no input parameters
+    public Book() {
+        this.title = "Master and Margarita";
+        this.author = "Unknown";
+        this.ISBN = 0000000;
+    }
+    //constructor with 3 input parameters
+    public Book(String titleFromInput, String authorFromInput, long ISBNFromInput) {
+        this.title = titleFromInput;
+        this.author = authorFromInput;
+        this.ISBN = ISBNFromInput;
+    }
+    //constructor with 2 input parameters
+    public Book(String titleFromInput, String authorFromInput) {
+        this.title = titleFromInput;
+        this.author = authorFromInput;
+        this.ISBN = 78965480;
+        }
+    }
+
+
--- src/BookLibrary.java
+++ src/BookLibrary.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BookLibrary {
+public class BookLibrary {
+    public static void main(String[] args) {
+
+        //creating instances with no parameters
+        Book firstPopular = new Book();
+
+        //creating instances with 3 parameter
+        Book secondPopular = new Book("Dog's heart", "Michael Bulgakov", 59867638);
+
+        //creating instances with 2 parameter
+        Book thirdPopular = new Book("Ivan Vasilievich", "M.Bulgakov");
+
+        System.out.println();
+        System.out.println("The second popular book is: " + secondPopular.title + ", Author: " + secondPopular.author + ", ISBN: " + secondPopular.ISBN);
+        System.out.println("The thrid popular book is: " + thirdPopular.title + ", " +thirdPopular.author + ", " + thirdPopular.ISBN);
+    }
 }
